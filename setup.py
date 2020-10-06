bb1bfaa8c2f7d50b2b18bd1f0c89618a0314fa11
Jussi Vatjus-Anttila <jussiva@gmail.com>
2020-10-06 13:00:48 +0300

    update lockable dependency (#12)

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     python_requires='>=3.7, <4',
     install_requires=[
         'pytest',
-        'lockable==0.2.0',
+        'lockable==0.3.0',
         'func_timeout',
         'filelock',
         'pydash'
