17ce805182a07948e723ec0f46562a2c1628dc01
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-03-16 14:36:36 +0200

    remove unused deps

--- setup.py
+++ setup.py
@@ -55,9 +55,7 @@ setup(
     python_requires='>=3.7, <4',
     install_requires=[
         'pytest',
-        'lockable==0.3.1',
-        'func_timeout',
-        'pydash'
+        'lockable==0.3.1'
     ],
     extras_require={  # Optional
         'dev': ['nose', 'coveralls', 'pylint', 'coverage'],
