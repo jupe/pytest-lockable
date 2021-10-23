79c00b3b3c005d089915f318bae1611077a6704a
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-10-23 10:07:26 +0300

    lockable_resource fixture yields now Allocation object (#22)

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     python_requires='>=3.7, <4',
     install_requires=[
         'pytest',
-        'lockable==0.6.0'
+        'lockable==0.7.0'
     ],
     extras_require={  # Optional
         'dev': ['nose', 'coveralls', 'pylint', 'coverage'],
