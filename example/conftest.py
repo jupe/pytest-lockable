3fcbc79b2e1510b21b96cf91241d2af77e890d0c
Jussi Vatjus-Anttila <jussiva@gmail.com>
2022-02-28 22:14:27 +0200

    add entrypoint for pytest_lockable.plugin (#26)

--- example/conftest.py
+++ example/conftest.py
@@ -1,8 +1,1 @@
 """ example conftest """
-import sys
-import os
-TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-PLUGIN_DIR = os.path.join(TEST_DIR, '../')
-sys.path.insert(0, PLUGIN_DIR)
-
-pytest_plugins = ("pytest_lockable.plugin", "metadata")  # pylint: disable=invalid-name
